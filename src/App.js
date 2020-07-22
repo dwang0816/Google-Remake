b3fae735f886d1fa9197bae1a33415ce5d83e538
Dennis Wang <dwang0816@gmail.com>
2020-07-22 14:20:34 -0400

    half way

--- src/App.js
+++ src/App.js
@@ -11,9 +11,24 @@ function App() {
           <a class="menu-item" href="/">Gmail</a>
           <a class="menu-item" href="/">Images</a>
           <i id="dropdown-menu" class="material-icons">apps</i>
-          <img alt="Profile Pic" src= {google}></img>
+          <img id="profile-pic" alt="Profile Pic" src= {google}></img>
         </nav>
       </header>
+
+      <main>
+        <section>
+          <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" id="google-logo"/>
+          <br/>
+          <div id="input-wrapper">
+            <input id="search-box"/>
+            <img src="https://www.gstatic.com/images/branding/googlemic/2x/googlemic_color_24dp.png" alt="Voice Search Icon"/>
+          </div>
+          <div>
+            <button class="custom-btn">Google Search</button>
+            <button class="custom-btn">I'm Feeling Lucky</button>
+          </div>
+        </section>
+      </main>
     </body>
   );
 }
