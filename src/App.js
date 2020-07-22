ca4e8ad958e48ee563c036753e8c5c3cba51153f
Dennis Wang <dwang0816@gmail.com>
2020-07-22 12:44:40 -0400

    added icon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import google from './google.png'
 
 function App() {
   return (
     <body>
       <header>
         <nav>
-          < a class="menu-item" href="/">Gmail</a>
-          <a href="/">Images</a>
-          
+          <a class="menu-item" href="/">Gmail</a>
+          <a class="menu-item" href="/">Images</a>
+          <i id="dropdown-menu" class="material-icons">apps</i>
+          <img alt="Profile Pic" src= {google}></img>
         </nav>
       </header>
     </body>
